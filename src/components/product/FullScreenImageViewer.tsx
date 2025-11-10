f965a75300636652372daae546db2dab793b9dea
HTG HUZAIFA <htghuzaifa2@gmail.com>
2025-11-10 19:40:53 +0000

    hey full screen not wokring  the image apear so small i wand it to be bi

--- src/components/product/FullScreenImageViewer.tsx
+++ src/components/product/FullScreenImageViewer.tsx
@@ -19,7 +19,7 @@ export function FullScreenImageViewer({ images, startIndex, open, onOpenChange }
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="p-0 border-0 bg-transparent w-screen h-screen max-w-none flex items-center justify-center">
+      <DialogContent className="p-0 border-0 bg-black/90 w-screen h-screen max-w-none flex items-center justify-center">
         <VisuallyHidden asChild>
             <DialogTitle>Full-screen Product Image Viewer</DialogTitle>
         </VisuallyHidden>
@@ -28,11 +28,11 @@ export function FullScreenImageViewer({ images, startIndex, open, onOpenChange }
             startIndex: startIndex,
             loop: true,
           }}
-          className="w-full h-full max-w-5xl max-h-[90vh]"
+          className="w-full h-full"
         >
-          <CarouselContent>
+          <CarouselContent className="h-full">
             {images.map((img, index) => (
-              <CarouselItem key={index}>
+              <CarouselItem key={index} className="h-full">
                 <div className="relative w-full h-full flex items-center justify-center p-4">
                   <Image
                     src={img.url}
@@ -48,8 +48,8 @@ export function FullScreenImageViewer({ images, startIndex, open, onOpenChange }
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious className="left-2 md:left-[-50px]" />
-          <CarouselNext className="right-2 md:right-[-50px]" />
+          <CarouselPrevious className="left-2 md:left-4 h-10 w-10 md:h-12 md:w-12" />
+          <CarouselNext className="right-2 md:right-4 h-10 w-10 md:h-12 md:w-12" />
         </Carousel>
       </DialogContent>
     </Dialog>
